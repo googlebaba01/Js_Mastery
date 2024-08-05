eab5b650d5c9d69a0a70d3a7603c4414064a4120
Gajendra Kumar Nagar <130752795+googlebaba01@users.noreply.github.com>
2024-08-05 20:11:44 +0000

    Operatios and conversions

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -5,7 +5,6 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
 
-
 // 33 => 33
 // 33abs => NaN
 // true => 1 
@@ -20,6 +19,42 @@ console.log(typeof boolisLoggedIn)
 // 1 => true , 0 => false
 // "" => false
 // "hitesh" => true
+
+/****************************Operations*******************************/
+console.log("1" + 2); // => 12
+console.log(1 + "2"); // => 12
+console.log(1+2 +"2"); // => 32
+console.log("1" + 2+2); // =>122
+
+console.log(+true); // => 1
+console.log(+""); // => 0
+
+let gameCounter = 100
+gameCounter++ // => 101
+++gameCounter // 102
+console.log(gameCounter);
+
+// Compression 
+
+console.log("2" > 1); // true
+console.log("02">1); // true
+
+console.log(null > 0); // false
+console.log(null == 0);//false
+console.log(null >= 0); // true => it converts null  to zero 
+// these are not best practices so avoid it write clean code only 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
