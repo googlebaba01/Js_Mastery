232241fd9b14d936d184292c46cff1f8601d0dd1
Gajendra Kumar Nagar <130752795+googlebaba01@users.noreply.github.com>
2024-08-05 18:24:40 +0000

    Conversionn of dataTypes

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// 33 => 33
+// 33abs => NaN
+// true => 1 
+
+let isLoggedIn = 1
+
+let boolisLoggedIn = Boolean(isLoggedIn)
+
+console.log(boolisLoggedIn)
+console.log(typeof boolisLoggedIn)
+
+// 1 => true , 0 => false
+// "" => false
+// "hitesh" => true
+
+
+
+
+
+
+
