aac3377a32c876a9b15954325070adc732ae0760
Gajendra Kumar Nagar <130752795+googlebaba01@users.noreply.github.com>
2024-08-08 17:12:34 +0000

    for interview purpose

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,37 @@
+// two types of dataTypes 
+// primittive -> pass by value
+// 7 -> string , boolean , null , undefined , number , symbol,BigInt
+//  you are champ i thing not need to explain about it 
+
+
+
+// NonPrimittive -> pass by referance 
+// arrays , objects , functions 
+// 1. Arrays 
+const heros = ["andy" ,"carlo" ,"victor" ,"sara"]
+// 2. objects 
+let myObj = {
+    name :"scout",
+    age:27,
+    profession:"gaming"
+}
+// 3. functions
+let myFunc = function(){
+    console.log("Hello Scout");   
+}
+console.log(myFunc);
+
+
+
+// *********** 
+
+// Primitive Data Type (Call by Value) => 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt. 
+// 1) The data type of null is an object.
+// 2) The data type of String is a string.
+// 3) The data type of Boolean is Boolean.
+// 4) The data type of Undefined is undefined.
+// 5) The data type of Symbol is a symbol.
+// 6) The data type of Number is number.
+// 7) The data type of BigInt is BigInt.
+// And the Data Types of Non primitive data type is Function object.
+
